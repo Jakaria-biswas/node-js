5f95b6e2bd9438eb9ce6dd94dbbd4e8fe82c1262
contactjakaria <contactjakaria95@gmail.com>
2021-02-15 12:36:28 +0600

    url request and get data server

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-      
-        res.end('hello world')
+
+       // get request through, url get data from server -simple way
+
+       if(req.url == '/'){
+                 res.writeHead(200, {'content-type': 'text/html'});
+                 res.write('<h1>this is home page</h1>');
+                 res.end()
+       }else if(req.url == '/about'){
+                res.writeHead(200, {'content-type':'text/html'})
+                res.write('<h1>this is about page</h1>');
+                res.end()
+       }else if(req){
+                res.writeHead(200, {'content-type':'text/html'})
+                res.write('<h1>this is contact page</h1>');
+                res.end()
+       }
+
+       
      
 });
 
