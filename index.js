18e2a21cae9b7237e140e482441f4f047e150ec3
contactjakaria <contactjakaria95@gmail.com>
2021-02-15 12:25:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');""
+
+const server = http.createServer((req,res) => {
+      
+        res.end('hello world')
+     
+});
+
+server.listen(7070);
+console.log('sever start')
